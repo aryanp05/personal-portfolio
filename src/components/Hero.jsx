72e1e3431d275a7ed2f7378b83e7840773cc8e42
Your Name <you@example.com>
2024-07-07 14:24:32 -0400

    minor changes to appear nicely on safari

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -91,7 +91,7 @@ const Hero = () => {
         variants={container(3)}
         initial="hidden"
         whileInView="visible"
-        className="relative text-2xl px-1 py-5  mb-12 cursor-pointer">
+        className="relative text-2xl px-1 py-5  mb-12 lg:mb-18 cursor-pointer">
           <h2 className="inline-block border border-2 border-white rounded upercase text-white py-2 px-12 font-medium hover:bg-gradient-to-r hover:from-yellow-400 hover:via-pink-500 hover:to-purple-500 bg-clip-text text-2xl tracking-tight hover:text-transparent hover:border-gray-400 transition duration-500 ease-in-out">
           🛠 Technologies 🛠
           </h2>
